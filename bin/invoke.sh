20b953a98a55f3dca71663947593d7095472c734
root@iotest <root@iotest>
2008-03-20 15:13:41 +0100

    document hp controller stuff

--- bin/invoke.sh
+++ bin/invoke.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 HOMEDIR=/root/benchmarks
+
 export LD=$HOMEDIR/logs-run-`date +%s`
 mkdir -p $LD
+
+hpacucli ctrl all show detail | tee $LD/hpacucli.log
+
 cd $HOMEDIR/bin
 sh bonnie.sh
 sh postgres.sh
