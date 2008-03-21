3d626892dc6cec3e6d344cc05b04eaec92331017
Christian Hofstaedtler <hofstaedtler@inqnet.at>
2008-03-21 13:19:07 +0100

    tunings for postgres benchmark; also replicate settings from Mr. Kaltenbrunner

--- bin/postgres2.sh
+++ bin/postgres2.sh
@@ -2,28 +2,30 @@
 
 mount -a
 
-/etc/init.d/postgresql-8.3 stop
-/etc/init.d/postgresql-8.3 start
+/etc/init.d/postgresql-8.3 stop && sleep 5
+/etc/init.d/postgresql-8.3 start && sleep 5
 
 dropdb bench
 createdb bench
 
 /usr/lib/postgresql/8.3/bin/pgbench -i bench -s 100
+echo 'CHECKPOINT; ' | psql bench
 
 LF=$1
 SLEEP=120
 
-/etc/init.d/postgresql-8.3 stop
+/etc/init.d/postgresql-8.3 stop 
 echo Sync, sleep.
 sync; sleep $SLEEP
 
 date
 date +%s
-for x in 1 2 3; do
+for x in 1; do
 
-	/etc/init.d/postgresql-8.3 start
+	/etc/init.d/postgresql-8.3 start && sleep 5
 
-	/usr/lib/postgresql/8.3/bin/pgbench -t 10000 -c 4 bench | tee $LF.$x.log
+	/usr/lib/postgresql/8.3/bin/pgbench -t 100000 -c 100 bench | tee $LF.$x.log
+	echo 'CHECKPOINT; ' | psql bench
 
 	/etc/init.d/postgresql-8.3 stop
 	echo Sync, sleep.
